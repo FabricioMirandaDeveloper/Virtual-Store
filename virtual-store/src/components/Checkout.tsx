2049ce9d51f4c9a8236f8b1f579d2d2fd59ccb5a
Italiano <mirandaquezadafabriciojoel@gmail.com>
2024-06-02 18:37:39 -0500

    trabajando con el local storage y el usestate

--- virtual-store/src/components/Checkout.tsx
+++ virtual-store/src/components/Checkout.tsx
@@ -1,7 +1,26 @@
-import styles from "./Checkout.module.css"
+import styles from "./Checkout.module.css";
 import { useState } from "react";
-export function Checkout({product}) {
-  const [quantity, setQuantity] = useState(1)
+export function Checkout({ product }) {
+  const [quantity, setQuantity] = useState(1);
+  const [button, setButton] = useState(false);
+  let productsInStorage = [];
+  !localStorage.getItem("cart")
+    ? localStorage.setItem("cart", JSON.stringify([]))
+    : (productsInStorage = JSON.parse(localStorage.getItem("cart")));
+  const manageCart = () => {
+    const one = productsInStorage.find((each) => each.id === product.id);
+    if (!one) {
+      productsInStorage.push(product);
+      setButton(true);
+    } else {
+      productsInStorage = productsInStorage.filter(
+        (each) => each.id !== product.id
+      );
+      setButton(false);
+    }
+    localStorage.setItem("cart", JSON.stringify(productsInStorage));
+  };
+
   return (
     <>
       <div className={styles["product-checkout-block"]}>
@@ -35,9 +54,19 @@ export function Checkout({product}) {
           </ul>
           <div className={styles["checkout-process"]}>
             <div className={styles["top"]}>
-              <input id="input-quantity" type="number" min="1" defaultValue={quantity} onChange={(event) => setQuantity(Number(event?.target.value))}/>
-              <button type="button" className={styles["cart-btn"]}>
-                Añadir al Carrito
+              <input
+                id="input-quantity"
+                type="number"
+                min="1"
+                defaultValue={quantity}
+                onChange={(event) => setQuantity(Number(event?.target.value))}
+              />
+              <button
+                type="button"
+                className={button ? styles["remove-btn"] : styles["cart-btn"]}
+                onClick={manageCart}
+              >
+                {button ? "Remove from cart" : "Add to cart"}
               </button>
             </div>
           </div>
