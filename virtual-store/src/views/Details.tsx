50b8721e463953ea8ec88f61b720e8b5adc3cb7f
Fabricio <mirandaquezadafabriciojoel@gmail.com>
2024-06-11 16:41:55 -0500

    modificando los breakpoints de ofertas

--- virtual-store/src/views/Details.tsx
+++ virtual-store/src/views/Details.tsx
@@ -1,6 +1,5 @@
 import { Navbar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
-import styles from "./Details.module.css";
 import products from "../assets/products";
 import { useParams } from "react-router-dom";
 import { NotFound } from "./NotFound.js";
@@ -17,22 +16,37 @@ export function Details() {
   if (product) {
     return (
       <>
-        <Navbar />
-        <main className="
+        {/* <Navbar /> */}
+        <main
+          className="
           w-full flex justify-center 
           items-center p-5
-        ">
+        "
+        >
           <div>
             <div id="details" className="flex">
-              <Thumbs product={product}/>
-              <Description product={product}/>
-              <Checkout product={product}/>
+              <Thumbs product={product} />
+              <Description product={product} />
+              <Checkout product={product} />
             </div>
-            <div className="flex flex-col">
-              <h2 className="text-center text-[40px]">Ofertas de la semana</h2>
+            <div className="flex flex-col w-full">
+              <h2 className="
+              text-center
+              text-xl
+              sm:text-2xl
+              md:text-3xl
+              lg:text-4xl
+              my-5
+              ">Ofertas de la semana</h2>
               <div
                 id="product-container"
-                className={styles["product-container"]}
+                className="w-full grid gap-4
+                   grid-cols-1
+                   sm:grid-cols-2
+                   md:grid-cols-3
+                   lg:grid-cols-4
+                   justify-items-center
+                   "
               >
                 {onsale.map((product) => (
                   <ProductCard
