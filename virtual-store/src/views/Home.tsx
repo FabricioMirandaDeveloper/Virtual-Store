af35102ae4906b51e24b945e752aa5c1a3b69a60
Italiano <mirandaquezadafabriciojoel@gmail.com>
2024-06-22 23:20:43 -0500

    modificando la vista home

--- virtual-store/src/views/Home.tsx
+++ virtual-store/src/views/Home.tsx
@@ -2,7 +2,6 @@ import { Navbar } from "../components/NavBar";
 import { Hero } from "../components/Hero";
 import { Footer } from "../components/Footer";
 import { ProductCard } from "../components/ProductCard";
-/* import products from "../assets/products.js"; */
 import Product from "../interfaces/product.js";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -14,7 +13,7 @@ export function Home() {
        .then((res) => setProducts(res.data))
        .catch((err) => console.log(err));
   }, []);
- 
+  
   return (
     <>
       <Navbar />
